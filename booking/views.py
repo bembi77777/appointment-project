0c8069dbe65c01f337c9cc4d20d5924045fae32a
Binderiya Munkhbat <2963376m@student.gla.ac.uk>
2026-05-27 04:08:37 +0100

    Added duplicate booking prevention

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import AppointmentForm
-
+from .models import Appointment
 def home(request):
+    form = AppointmentForm(request.POST)
 
-    if request.method == 'POST':
-        form = AppointmentForm(request.POST)
+    if form.is_valid():
 
-        if form.is_valid():
+        appointment = form.cleaned_data
+
+        existing = Appointment.objects.filter(
+            appointment_date=appointment['appointment_date'],
+            appointment_time=appointment['appointment_time']
+        ).exists()
+
+        if existing:
+            messages.error(
+                request,
+                "This appointment slot is already booked."
+            )
+        else:
             form.save()
             messages.success(
                 request,
